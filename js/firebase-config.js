614072aae5ea59af8896717daaab40f255406c4e
captainbmw <drillerafro@gmail.com>
2026-05-28 21:21:24 +0700

    greenworld

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Realtime Database configuration and initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB9AanYqJf9nWT44xbs7LYIk_C9SsKe-kI",
     authDomain: "greenworldstore-e00c4.firebaseapp.com",
-    // Note: databaseURL is required for Realtime Database
     databaseURL: "https://greenworldstore-e00c4-default-rtdb.firebaseio.com/",
     projectId: "greenworldstore-e00c4",
     storageBucket: "greenworldstore-e00c4.firebasestorage.app",
@@ -17,5 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const storage = getStorage(app);
 
-export { db };
+export { db, storage };
